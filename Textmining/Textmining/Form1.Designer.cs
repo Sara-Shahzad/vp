f1a77b52229f6ab59d184281c26de2f8e35f9b48
Sara-Shahzad <sarashahzad54@gmail.com>
2016-04-21 23:12:13 +0500

    Adding a word file to form

---
+++ Textmining/Textmining/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Textmining
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnadd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchbox = new System.Windows.Forms.TextBox();
+            this.path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(203, 47);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(77, 20);
+            this.btnadd.TabIndex = 0;
+            this.btnadd.Text = "Browse";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Word to Search";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // searchbox
+            // 
+            this.searchbox.Location = new System.Drawing.Point(12, 111);
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(176, 20);
+            this.searchbox.TabIndex = 2;
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(12, 47);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(176, 20);
+            this.path.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter File Path";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 277);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.searchbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnadd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchbox;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Textmining/Textmining/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Textmining
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd1 = new OpenFileDialog();
+            fd1.Filter = "(*.docx) |*.docx";
+            if(fd1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.path.Text = fd1.FileName;
+
+            }
+            MessageBox.Show("Your file has been added.");
+        }
+    }
+}
