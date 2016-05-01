288346ab947872810ad2117e40ca41812aaee0cd
Sara-Shahzad <sarashahzad54@gmail.com>
2016-05-01 18:04:01 +0500

    used Regex, showed doc in pdf, interface

--- Textmining/Textmining/Form1.Designer.cs
+++ Textmining/Textmining/Form1.Designer.cs
@@ -36,12 +36,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.pdf = new AxAcroPDFLib.AxAcroPDF();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pdf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnadd
             // 
-            this.btnadd.Location = new System.Drawing.Point(203, 47);
+            this.btnadd.Location = new System.Drawing.Point(12, 98);
             this.btnadd.Name = "btnadd";
             this.btnadd.Size = new System.Drawing.Size(77, 20);
             this.btnadd.TabIndex = 0;
@@ -52,31 +58,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 95);
+            this.label1.Location = new System.Drawing.Point(9, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.Size = new System.Drawing.Size(76, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Enter Word to Search";
+            this.label1.Text = "Enter Keyword";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // searchbox
             // 
-            this.searchbox.Location = new System.Drawing.Point(12, 111);
+            this.searchbox.Location = new System.Drawing.Point(12, 158);
             this.searchbox.Name = "searchbox";
-            this.searchbox.Size = new System.Drawing.Size(176, 20);
+            this.searchbox.Size = new System.Drawing.Size(139, 20);
             this.searchbox.TabIndex = 2;
             // 
             // path
             // 
-            this.path.Location = new System.Drawing.Point(12, 47);
+            this.path.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.path.Location = new System.Drawing.Point(12, 72);
             this.path.Name = "path";
-            this.path.Size = new System.Drawing.Size(176, 20);
+            this.path.Size = new System.Drawing.Size(139, 20);
             this.path.TabIndex = 3;
+            this.path.TextChanged += new System.EventHandler(this.path_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 31);
+            this.label2.Location = new System.Drawing.Point(9, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 4;
@@ -84,7 +92,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(203, 111);
+            this.button1.Location = new System.Drawing.Point(12, 184);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -95,17 +103,67 @@
             // pdf
             // 
             this.pdf.Enabled = true;
-            this.pdf.Location = new System.Drawing.Point(12, 164);
+            this.pdf.Location = new System.Drawing.Point(157, 40);
             this.pdf.Name = "pdf";
             this.pdf.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pdf.OcxState")));
-            this.pdf.Size = new System.Drawing.Size(523, 215);
+            this.pdf.Size = new System.Drawing.Size(405, 235);
             this.pdf.TabIndex = 6;
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.SystemColors.Info;
+            this.Result.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Result.Location = new System.Drawing.Point(12, 281);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(550, 140);
+            this.Result.TabIndex = 7;
+            this.Result.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Textmining.Properties.Resources.search_xxl;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(153, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 34);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Textmining.Properties.Resources.black_search_icon_transparent_background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.ErrorImage = global::Textmining.Properties.Resources.black_search_icon_transparent_background;
+            this.pictureBox1.InitialImage = global::Textmining.Properties.Resources.black_search_icon_transparent_background;
+            this.pictureBox1.Location = new System.Drawing.Point(132, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(19, 20);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bernard MT Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(207, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 34);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "TEXT MINING";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(555, 395);
+            this.ClientSize = new System.Drawing.Size(574, 433);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Result);
             this.Controls.Add(this.pdf);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -113,9 +171,12 @@
             this.Controls.Add(this.searchbox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnadd);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Text Mining";
             ((System.ComponentModel.ISupportInitialize)(this.pdf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,6 +191,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private AxAcroPDFLib.AxAcroPDF pdf;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Textmining/Textmining/Form1.cs
+++ Textmining/Textmining/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Word;
+using System.Text.RegularExpressions;
 
 
 namespace Textmining
@@ -32,46 +33,48 @@ namespace Textmining
             if (fd1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 this.path.Text = fd1.FileName;
-                file=this.path.Text;
+                file = this.path.Text;
+                String outfile = "D:\\file.pdf";
+
+                Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+                Document doc = null;
+                doc = app.Documents.Open(file, Type.Missing, false);
+                doc.ExportAsFixedFormat(outfile, WdExportFormat.wdExportFormatPDF);
+                doc.Close(false, Type.Missing, Type.Missing);
+                app.Quit(false, false, false);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+                pdf.src = outfile;
                 MessageBox.Show("Your file has been added.");
-
             }
-            //String outfile = "D:\\file.pdf";
-            
-            //Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
-            //Document doc = null;
-            //doc = app.Documents.Open(file , Type.Missing ,false);
-            //doc.ExportAsFixedFormat(outfile, WdExportFormat.wdExportFormatPDF);
-            //doc.Close(false, Type.Missing, Type.Missing);
-            //app.Quit(false, false, false);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
-            //pdf.src = outfile;
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             String a = searchbox.Text;
-            int b = 0;
-            
-            Microsoft.Office.Interop.Word.Application application = new Microsoft.Office.Interop.Word.Application();
-            Document document = application.Documents.Open(path.Text);
-            int count = document.Words.Count;
-            for (int i = 1; i <= count; i++)
+            String paths = path.Text;
+            Class1 c=new Class1();
+
+            int num = c.SearchWord(a, paths);
+            if(num>0)
             {
-                //Console.WriteLine(count);
-
-               
-                string text = document.Words[i].Text;
-                //Console.WriteLine(text);
-                if (text == a)
-                {
-                    b++;
-
-                }
+                string xyz = "Found...\n\n" + "number of times the KeyWord Appeared in the document is " + num+"...";
+                Result.Text = xyz;
             }
-            string xyz = "number of words found = " + b;
-            MessageBox.Show(xyz);
+            else
+            {
+                Result.Text = "Not Found.....\n";
+            }
+            
+        }
+
+        private void path_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
